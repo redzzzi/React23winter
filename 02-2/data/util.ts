b9320da265f2a5db3a54e0052a5986006f973637
redzzzi <myqueen1346@naver.com>
2024-03-23 19:32:17 +0900

    del 02-2

--- 02-2/data/util.ts
+++
@@ -1,6 +0,0 @@
-//가짜데이터를만드는함수정의하기
-export const makeArray = (length: number) => new Array(length).fill(null)
-export const range = (min: number, max: number): number[] =>
-	makeArray(max - min).map((notUsed, index) => index + min)
-export const random = (min: number, max: number): number =>
-	Math.floor(Math.random() * (max - min)) + min
