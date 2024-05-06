41ffe4c113f9319a565dd6877e0edf6c9ad763ad
redzzzi <myqueen1346@naver.com>
2024-05-07 03:00:19 +0900

    onClick button

--- 02-5/src/pages/ReactOnClick.tsx
+++ 02-5/src/pages/ReactOnClick.tsx
@@ -1,3 +1,14 @@
+import type {SyntheticEvent} from 'react'
+
 export default function ReactOnClick() {
-  return <div>ReactOnClick</div>
+  const onClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles} = e
+    console.log('mouse click occurs on <button>', isTrusted, target, bubbles)
+  }
+  return (
+    <div>
+      <p>ReactOnClick</p>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  )
 }
