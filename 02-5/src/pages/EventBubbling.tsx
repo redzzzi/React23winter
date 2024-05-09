7fdb799c3c67371411441e0c525e50ad3e1c2124
redzzzi <myqueen1346@naver.com>
2024-05-10 08:24:23 +0900

    update EventBubbling.tsx

--- 02-5/src/pages/EventBubbling.tsx
+++ 02-5/src/pages/EventBubbling.tsx
@@ -1,3 +1,18 @@
+import type {SyntheticEvent} from 'react'
+
 export default function EventBubbling() {
-  return <div>EventBubbling</div>
+  const onDivClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles, currentTarget} = e
+    console.log('click event bubbles on <div>', isTrusted, target, bubbles, currentTarget)
+  }
+  const onButtonClick = (e: SyntheticEvent) => {
+    const {isTrusted, target, bubbles} = e
+    console.log('click event starts at <button>', isTrusted, target, bubbles)
+  }
+  return (
+    <div onClick={onDivClick}>
+      <p>EventBubbling</p>
+      <button onClick={onButtonClick}>Click Me</button>
+    </div> 
+  )
 }
