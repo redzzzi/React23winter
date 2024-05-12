6939a2ace31f69514d649d987b8511c0c476e780
redzzzi <myqueen1346@naver.com>
2024-05-13 00:46:51 +0900

    type='file'

--- 02-5/src/pages/FileInput.tsx
+++ 02-5/src/pages/FileInput.tsx
@@ -1,3 +1,19 @@
+import type {ChangeEvent} from 'react'
+
 export default function FileInput() {
-  return <div>FileInput</div>
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files: FileList | null = e.target.files
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File | null = files.item(i) // or        file = files[i]
+        console.log('file[${i}]: ', file)
+      }
+    }
+  }
+  return (
+    <div>
+      <p>FileInput</p>
+      <input type="file" onChange={onChange} multiple accept="image/*" />
+    </div>
+  )
 }
