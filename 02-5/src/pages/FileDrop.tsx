ad7d284d9ed76087bdea82079756a3a44413adb8
redzzzi <myqueen1346@naver.com>
2024-05-14 16:14:47 +0900

    FileDrop component

--- 02-5/src/pages/FileDrop.tsx
+++ 02-5/src/pages/FileDrop.tsx
@@ -1,3 +1,25 @@
+import type {DragEvent} from 'react'
+
 export default function FileDrop() {
-  return <div>FileDrop</div>
+  const onDragOver = (e: DragEvent) => e.preventDefault()
+
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    const files = e.dataTransfer.files
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        const file: File | null = files.item(i)
+        console.log(`files[${i}]`, file)
+      }
+    }
+  }
+
+  return (
+    <div>
+      <p>FileDrop</p>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop image Files over Me</h1>
+      </div>
+    </div>
+  )
 }
